8bece1648e11a122973f53525f16f08837e186cf
Neal Varner <neal.varner@gmail.com>
2015-03-14 19:15:08 -0500

    This is a I'm confused commit. I can't seem to get guessMe in app.js to not regenerate itself with the receipt of each new action. Not sure if that makes sense.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+var guessMe;
 
 $(document).ready(function(){
-	
+    guessMe = $('#guessButton').data('guessMe');
+    if (!guessMe) {
+        guessMe = $('#guessButton').data('guessMe', genRandomInt(0, 100));
+    }
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +16,46 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    //process a guess
+    $('#guessButton').click(function(){
+        //get userGuess
+        userGuess = $('#userGuess').val();
+        console.log(userGuess);
+        response = process_guess(userGuess, lastGuess, guessMe);
+        //set last guess
+        lastGuess = userGuess;
+
+        var count = $('#count').val();
+        $('#count').text(count++);
+    });
 });
 
+// function startGame() {
+//     // generate random number to be guessed
+//     guessMe = getRandomInt(0, 100);
+//     //set text input to blank
+//     userGuess = $('#userGuess').val('');
 
+//     return guessMe;
+// }
+
+
+
+function genRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function process_guess(userGuess, lastGuess, guessMe) {
+    var response = '';
+    //change display to respond with hot or cold
+    if (userGuess < guessMe) {
+        response = 'less than';
+    }
+    else if (userGuess > guessMe) {
+        response = 'greater than';
+    }
+    else {
+        response = 'correct';
+    }
+    return response;
+}
